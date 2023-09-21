1e6f70fd03f33190b3e87eeb0c9f9a68fb3ba0d3
royandugu <royandugu99009@gmail.com>
2023-09-21 08:45:08 +0545

    Responsive menu

--- components/userComponents/primaryComponents/menuComponents/menuDisplay.tsx
+++ components/userComponents/primaryComponents/menuComponents/menuDisplay.tsx
@@ -1,23 +1,32 @@
 import NavigationContainer from "./navigationContainer";
+import {HiMiniBars3} from "react-icons/hi2";
 
 import "./menuDisplay.css";
 
 const MenuDisplay = () => {
     return (
-        <div className="menuDisplay fixed top-10 left-20 right-20 p-5 rounded-md shadow-lg" style={{borderTop:"1px solid black"}}>
-            <div className="flex flex-row" style={{alignItems:"center"}}>
+        <div className="menuDisplay fixed top-10 lg:left-20 lg:right-20 left-5 right-5 p-5 rounded-md shadow-lg" style={{borderTop:"1px solid black"}}>
+            <div className="flex flex-row items-center xs:justify-between lg:justify-start">
                 <div className="logoContainer">
                     <h5 className="title"> Logo </h5>
                 </div>
-                <div className="navigationContainer px-20 ml-20">
-                    <NavigationContainer />
+
+                <>
+                    <div className="navigationContainer lg:block hidden px-20 ml-20">
+                        <NavigationContainer />
+                    </div>
+                    <div className="searchBar lg:block hidden ">
+                        <h5> Search bar </h5>
+                    </div>
+                    <div className="contactUs lg:block hidden">
+                        <h5> Contact us </h5>
+                    </div>
+                </>
+
+                <div className="lg:hidden">
+                    <HiMiniBars3/>
                 </div>
-                <div className="searchBar">
-                    <h5> Search bar </h5>
-                </div>
-                <div className="contactUs">
-                    <h5> Contact us </h5>
-                </div>
+            
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,12 @@ module.exports = {
   content: [
     './**/*.{html,js,tsx}',
   ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '320px', // Define a custom breakpoint for screens smaller than sm
+      },
+    },
+  }
   // ...
 }

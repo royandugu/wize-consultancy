7de07716271acbf816da4a903dd9a1707b199f23
royandugu <royandugu99009@gmail.com>
2023-12-10 08:16:04 +0545

    image uploading div

--- components/systemComponents/loginAndRegister/register.tsx
+++ components/systemComponents/loginAndRegister/register.tsx
@@ -1,6 +1,8 @@
 "use client"
-import { useState } from "react";
+
+import { useState, useRef } from "react";
 import { universalFormPost } from "../apiConnectors/system/POST";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 import Link from "next/link";
 
@@ -15,7 +17,21 @@ export const Register=(prop:registerProp)=>{
     const [name,setName]=useState("");
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
-    
+    const [newImage ,setNewImage]=useState<any>(null);
+    const [image,setImage]=useState<any>();
+
+    const imageRef = useRef(null);
+
+    const trimmer = (path:string) => {
+        if (path) return path.replace('../public', '');
+    }  
+
+    const onImageChange = (event:any) => {
+        if (event.target.files && event.target.files[0]) {
+            setNewImage(event.target.files[0]);
+            setImage(URL.createObjectURL(event.target.files[0]));
+        }
+    }
 
     const registerUser=async (e:any)=>{
         e.preventDefault();
@@ -30,7 +46,14 @@ export const Register=(prop:registerProp)=>{
 
     return(
         <form className="loginAndRegisterForm" onSubmit={(e)=>registerUser(e)}>
-            <input type="file" onChange={(e)=>setProfile(e.target.files?.[0])}/><br/>
+            <label className="fileType cursor-pointer">
+                <div ref={imageRef} className="currentImgBackground h-[300px] w-[300px]" style={{ background: `url(${trimmer(image)})`}}>
+                    <div className="fileUpload--updateProfilePfp">
+                        <AiOutlinePlusCircle className="plusIcon" />
+                    </div>
+                </div>
+                <input type="file" className="hidden" onChange={onImageChange} />
+            </label>
             <input type="text" placeholder="Name" onChange={(e)=>setName(e.target.value)}/><br/>
             <input type="text" placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/><br/>
             <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/><br/>
